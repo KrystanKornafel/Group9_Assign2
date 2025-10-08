6d5e742b75be6ded9dfcdc530575f1be47de0c9e
Aeldesouki <ahmed.eldesouki2021@gmail.com>
2025-10-08 00:31:01 -0400

    Update Account.java

--- src/Group9_BankingSystem/Account.java
+++ src/Group9_BankingSystem/Account.java
@@ -5,33 +5,84 @@ import java.util.Scanner;
  * <p>Class: SEG2105
  * <p>Assignment: Assignment 2
  *
- * <p>Description: This is the Savings class, used to collect the information for the client's savings account.
+ * <p>Description: This is the Account class, used to store the data for each account.
  */
 
 
-public interface Account {
+public abstract class Account {
 
 	/** The parameter accountNum is used to get the account number for a particular client **/
-	private long acountNum;
-	/** The parameter nickname is used to get the nickname for a particular account **/
-	private String nickname;
-	/** The parameter interestRate is used to determine the interest rate for a particular account **/
-	private float interestRate;
+	private long accountNum;
 	/** Used to store the client's other information such as ageGroup and businessType **/
-	private Client client;
+	private Client[] clientArray;
+	private int numClients;
 	
-//	/** No arg constructor*/
-//	public Account () {
-//	}
-//	
-//	/**
-//	 * Parameterized constructor to allow data to be passed into the object. The data can then be used to create the checking account for a particular client.
-//	 * @param accountNum is used to get the account number for the client
-//	 * @param nickname is used to get the nickname for the client's account
-//	 */
-//	public Account (long accountNum, String nickname,  Client client) {
-//		this.accountNum = accountNum;
-//		this.nickname = nickname;
-//		this.client = client;
-//	}
+	/** 
+	 * No-arg constructor
+	 */
+	public Account() {
+		
+	}
+	
+
+	/** 
+	 * Parameterized constructor
+	 */
+	protected Account(long accountNum, Client client) {
+		this.accountNum = accountNum;
+		this.clientArray = new Client[1];
+		this.clientArray[0] = client;
+		this.numClients = 1;
+	}
+	
+	//getters and setters for accountNum
+	protected long getAccountNum(){
+		return accountNum;
+	}
+	protected void setAccountNum(long accountNum) {
+		this.accountNum = accountNum;
+	}
+	
+	protected void addClient(Client client) {
+		if (numClients == 8) {
+			System.out.println("Error: Too many clients");
+		} else {
+			numClients++;
+			Client[] newArray = new Client[numClients];
+			for (int i = 0; i < numClients - 1; i++) {
+				newArray[i] = clientArray[i];
+			}
+			newArray[numClients-1] = client;
+			clientArray = newArray;
+		}
+	}
+	protected void removeClient() {
+		if (numClients == 1) {
+			System.out.println("Error: Too few clients");
+		}
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Please enter the client number of the client you wish to remove: ");
+		int n = reader.nextInt();
+		reader.close();
+		for (int i = 0; i < numClients; i++) {
+			if (clientArray[i].getClientNumber() == n) {
+				numClients--;
+				Client[] newArray = new Client[numClients];
+				int k = 0;
+				for (int j = 0; j < numClients - 1; j++) {
+					if (j == i) { k++; }
+					newArray[j] = clientArray[k];
+					k++;
+				}
+				clientArray = newArray;
+				return;
+			}
+		}
+		System.out.println("Error: Invalid client number");
+	}
+	protected void listClients() {
+		for (int i = 0; i < numClients; i++) {
+			System.out.println("Client " + i + ": " + clientArray[i].getName() + " (#" + clientArray[i].getClientNumber() + ")");
+		}
+	}
 }
