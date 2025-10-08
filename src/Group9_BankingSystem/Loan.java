c777496aba068b80fd415bc5eb96ac356a9bdf71
Aeldesouki <ahmed.eldesouki2021@gmail.com>
2025-10-08 00:33:44 -0400

    Update Loan.java

--- src/Group9_BankingSystem/Loan.java
+++ src/Group9_BankingSystem/Loan.java
@@ -8,20 +8,11 @@ import java.util.Scanner;
  * <p>Description: This is the Loan class, used to collect the information of each loan a client has.
  */
 //This class implements the ___ class
-class Loan {
+public class Loan extends Account {
 	
-	/** The parameter accountNum is used to get the account number for a particular client **/
-	private long acountNum;
 	/** The parameter interestRate is used to determine the interest rate for a particular account **/
 	private float interestRate;
 	
-	/** Used to store the client's other information such as ageGroup and businessType **/
-	private Client client;
-	
-	
-	/** No arg constructor*/
-	public Loan () {
-	}
 	
 	/**
 	 * Parameterized constructor to allow data to be passed into the object. The data can then be used to create the checking account for a particular client.
@@ -30,28 +21,17 @@ class Loan {
 	 * @param client is used to get the information on the client object 
 	 */
 	public Loan (long accountNum, float interestRate, Client client) {
-		this.accountNum = accountNum;
+		super(accountNum, client); //Inherits this number from the Account super class
 		this.interestRate = interestRate;
-		this.client = client;
 	}
 
 	
-	//Getters and setters for the client's account number 
-	protected getAccountNum(){
-		return accountNum;
-	}
-	protected void setAccountNum(long accountNum) {
-		this.accountNum = accountNum; //return this instance of accountNum
-	}
-	
 	//Getters and setters for account's interest rate
-	protected getInterestRate(){
+	public float getInterestRate(){
 		return interestRate;
 	}
-	protected void setInterestRate(float interestRate) {
+	public void setInterestRate(float interestRate) {
 		this.interestRate = interestRate;
 	}
-	
-	//put the add and remove methods here
-	
+
 }
