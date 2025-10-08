d64d27db54abc52810aac362bd61f242033ecdc9
Aeldesouki <ahmed.eldesouki2021@gmail.com>
2025-10-08 00:33:25 -0400

    Update Investment.java

--- src/Group9_BankingSystem/Investment.java
+++ src/Group9_BankingSystem/Investment.java
@@ -8,19 +8,11 @@ import java.util.Scanner;
  * <p>Description: This is the Investment class, used to collect the information of each investment a client has.
  */
 //This class implements the ___ class
-class Investment {
+public class Investment extends Account {
 	
-	/** The parameter accountNum is used to get the account number for a particular client **/
-	private long acountNum;
 	/** The parameter nickname is used to get the nickname for a particular account **/
 	private String nickname;
-	/** Used to store the client's other information such as ageGroup and businessType **/
-	private Client client;
 
-	/** No arg constructor*/
-	public Investment () {
-	}
-	
 	/**
 	 * Parameterized constructor to allow data to be passed into the object. The data can then be used to create the checking account for a particular client.
 	 * @param accountNum is used to get the account number for the client
@@ -28,29 +20,18 @@ class Investment {
 	 * @param client is used to get the information on the client object 
 	 */
 	public Investment (long accountNum, String nickname, Client client) {
-		this.accountNum = accountNum;
+		super(accountNum, client); //Inherits this number from the Account super class
 		this.nickname = nickname;
-		this.client = client;
-	}
-
-	
-	//Getters and setters for the client's account number 
-	protected getAccountNum(){
-		return accountNum;
-	}
-	protected void setAccountNum(long accountNum) {
-		this.accountNum = accountNum; //return this instance of accountNum
 	}
 	
 	//Getters and setters for the nickname of a particular account 
-	protected getNickname(){
+	public String getNickname(){
 		return nickname;
 	}
-	protected void setNickname(String nickname) {
+	public void setNickname(String nickname) {
 		this.nickname = nickname;
 	}
 	
-	//put the add and remove methods here
 	
 }
 
